344c8fcfb0221f6ee4c7c36674ceb4a931cbc396
deadletterb <40750668+deadletterb@users.noreply.github.com>
2021-01-02 21:48:49 -0800

    Add files via upload

--- Camera.cs
+++ Camera.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+using System.Threading;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Math;
 
 public class Camera : MonoBehaviour
 {
@@ -13,24 +16,57 @@ public class Camera : MonoBehaviour
     void Update()
     {
       // find centre of the graph
-      float minx = 1000000.0f; float maxx = 0.0f;
-      float miny = 1000000.0f; float maxy = 0.0f;
-      float minz = 1000000.0f; float maxz = 0.0f;      
+      float _count_elements_ = 0.0f;
+      double radius = 0.0d;
+      float xmean = 0.0f;
+      float ymean = 0.0f;
+      float zmean = 0.0f;
+      double deltax = 0.0d;
+      double deltay = 0.0d;
+      double deltaz = 0.0d;
+      double theta = 0.0d;
+
       foreach (Transform child in graph.transform){
-	if (child.position.x < minx) minx = child.position.x;
-	if (child.position.x > maxx) maxx = child.position.x;
-	if (child.position.y < miny) miny = child.position.y;
-	if (child.position.y > maxy) maxy = child.position.y;
-	if (child.position.z < minz) minz = child.position.z;
-	if (child.position.z > maxz) maxz = child.position.z;			
+      xmean += child.position.x;
+      ymean += child.position.y;
+      zmean += child.position.z;
+      _count_elements_ += 1;
       }
-      transform.LookAt(new Vector3((minx+maxx)/2,(miny+maxy)/2,(minz+maxz)/2));
+      
+      xmean /= _count_elements_;
+      ymean /= _count_elements_;
+      zmean /= _count_elements_;
+      
+      deltax = transform.position.x  - xmean;
+      deltay = transform.position.y  - ymean;
+      deltaz = transform.position.z  - zmean;
+      radius = Sqrt(Pow(deltax,2f) + Pow(deltaz,2f)); /* note that radius here does NOT include vertical, only flat in the plane */
+      theta = Atan2(deltaz , deltax);  
+
+      UnityEngine.Debug.Log("camera delta [x, y, z] = [" + deltax + ", " + deltay + ", " + deltaz + "], radius =" + radius + ", theta = " + theta); 
+      
+      transform.LookAt(new Vector3(xmean, ymean, zmean));
+      
       if (Input.GetKey("up")){
 	transform.position += new Vector3(0,0,Time.deltaTime*speed);
       }
+
       if (Input.GetKey("down")){
 	transform.position -= new Vector3(0,0,Time.deltaTime*speed);
-      }      
+      }
+      if (Input.GetKey("a")){
+	transform.position += new Vector3((float)(-radius * Cos(theta) + radius * Cos (theta + PI /180)), 0 , (float)(-radius * Sin(theta) + radius * Sin (theta + PI /180) ));
+      }
+      if (Input.GetKey("d")){
+	transform.position += new Vector3((float)(-radius * Cos(theta) + radius * Cos (theta - PI /180)), 0 , (float)(-radius * Sin(theta) + radius * Sin (theta - PI /180) ));
+      }
+            if (Input.GetKey("w")){
+	transform.position += new Vector3(0,Time.deltaTime*speed,0);
+      }
+
+      if (Input.GetKey("s")){
+	transform.position -= new Vector3(0,Time.deltaTime*speed,0);
+      }
     }
     
-}
+} 
